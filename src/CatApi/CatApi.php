91585a0cc6455de9d95eeb69a3240e0341a9adf7
Marcos Palacios Rovira <fcoparo@gmail.com>
2016-12-05 17:53:14 +0100

    6- Eliminar if else

--- src/CatApi/CatApi.php
+++ src/CatApi/CatApi.php
@@ -7,27 +7,30 @@ class CatApi {
     const THREE_SECONDS_OF_LIFE = 3;
 
     public function getRandomImage() {
-   //       if (!file_exists(__DIR__ . '/../../cache/random') || $this->getFileHowOldIsIt() > self::THREE_SECONDS_OF_LIFE) {
-        if (!$this->fileRandomExist() || $this->getFileHowOldIsIt() > self::THREE_SECONDS_OF_LIFE) {
-
-            $responseXml = @file_get_contents(
-                            'http://thecatapi.com/api/images/get?format=xml&type=jpg'
-            );
-            if (!$responseXml) {
-                // the cat API is down or something
-                return 'http://cdn.my-cool-website.com/default.jpg';
-            }
-
-            $responseElement = new \SimpleXMLElement($responseXml);
-
-            file_put_contents(
-                    __DIR__ . '/../../cache/random', (string) $responseElement->data->images[0]->image->url
-            );
-
-            return (string) $responseElement->data->images[0]->image->url;
-        } else {
+        if ($this->fileRandomExist() && $this->getFileHowOldIsIt() <= self::THREE_SECONDS_OF_LIFE) {
             return file_get_contents(__DIR__ . '/../../cache/random');
         }
+
+        //       if (!$this->fileRandomExist() || $this->getFileHowOldIsIt() > self::THREE_SECONDS_OF_LIFE) {
+
+        $responseXml = @file_get_contents(
+                        'http://thecatapi.com/api/images/get?format=xml&type=jpg'
+        );
+        if (!$responseXml) {
+            // the cat API is down or something
+            return 'http://cdn.my-cool-website.com/default.jpg';
+        }
+
+        $responseElement = new \SimpleXMLElement($responseXml);
+
+        file_put_contents(
+                __DIR__ . '/../../cache/random', (string) $responseElement->data->images[0]->image->url
+        );
+
+        return (string) $responseElement->data->images[0]->image->url;
+//        } else {
+//            return file_get_contents(__DIR__ . '/../../cache/random');
+//        }
     }
 
     protected function getFileHowOldIsIt() {
--- tests/CatApi/CatApiTest.php
+++ tests/CatApi/CatApiTest.php
@@ -4,16 +4,14 @@ namespace CatApi\Tests;
 
 use CatApi\CatApi;
 
-class CatApiTest extends \PHPUnit_Framework_TestCase
-{
-    protected function tearDown()
-    {
+class CatApiTest extends \PHPUnit_Framework_TestCase {
+
+    protected function tearDown() {
         @unlink(__DIR__ . '/../../cache/random');
     }
 
     /** @test */
-    public function it_fetches_a_random_url_of_a_cat_gif()
-    {
+    public function it_fetches_a_random_url_of_a_cat_gif() {
         $catApi = new CatApi();
 
         $url = $catApi->getRandomImage();
@@ -22,14 +20,14 @@ class CatApiTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    public function it_caches_a_random_cat_gif_url_for_3_seconds()
-    {
+    public function it_caches_a_random_cat_gif_url_for_3_seconds() {
+        
         $catApi = new CatApi();
 
         $firstUrl = $catApi->getRandomImage();
-        sleep(2);
+        sleep(1);
         $secondUrl = $catApi->getRandomImage();
-        sleep(2);
+        sleep(3);
         $thirdUrl = $catApi->getRandomImage();
 
         $this->assertSame($firstUrl, $secondUrl);
