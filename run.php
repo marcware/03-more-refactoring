c68099ced3ccedb98d56b6ec010fdbd2cc8a33e4
Marcos Palacios Rovira <fcoparo@gmail.com>
2016-12-05 17:19:51 +0100

    4- Creamos el método para calcular la diferencia entre la creación del fichero y ahora

--- run.php
+++ run.php
@@ -5,6 +5,7 @@ require __DIR__  . '/vendor/autoload.php';
 use CatApi\CatApi;
 
 $catApi = new CatApi();
+echo $catApi->getRandomImage();
 
-echo 'URL for cat gif with id "vd": ' . $catApi->getCatGifUrl('vd') . "\n";
-echo 'A random URL of a cat gif: ' . $catApi->getRandomCatGifUrl() . "\n";
+//echo 'URL for cat gif with id "vd": ' . $catApi->getCatGifUrl('vd') . "\n";
+//echo 'A random URL of a cat gif: ' . $catApi->getRandomCatGifUrl() . "\n";
--- src/CatApi/CatApi.php
+++ src/CatApi/CatApi.php
@@ -4,12 +4,16 @@ namespace CatApi;
 
 class CatApi {
 
+    const THREE_SECONDS_OF_LIFE = 3;
+
     public function getRandomImage() {
-        if (!file_exists(__DIR__ . '/../../cache/random') || time() - filemtime(__DIR__ . '/../../cache/random') > 3) {
+//        if (!file_exists(__DIR__ . '/../../cache/random') 
+//                || time() - filemtime(__DIR__ . '/../../cache/random') > WAIT_TREE_SECONDS) {
+        if (!file_exists(__DIR__ . '/../../cache/random') || $this->getFileHowOldIsIt() > self::THREE_SECONDS_OF_LIFE) {
+
             $responseXml = @file_get_contents(
                             'http://thecatapi.com/api/images/get?format=xml&type=jpg'
             );
-
             if (!$responseXml) {
                 // the cat API is down or something
                 return 'http://cdn.my-cool-website.com/default.jpg';
@@ -18,8 +22,7 @@ class CatApi {
             $responseElement = new \SimpleXMLElement($responseXml);
 
             file_put_contents(
-                    __DIR__ . '/../../cache/random'
-                    , (string) $responseElement->data->images[0]->image->url
+                    __DIR__ . '/../../cache/random', (string) $responseElement->data->images[0]->image->url
             );
 
             return (string) $responseElement->data->images[0]->image->url;
@@ -27,5 +30,11 @@ class CatApi {
             return file_get_contents(__DIR__ . '/../../cache/random');
         }
     }
+
+    public function getFileHowOldIsIt() {
+        return time() - filemtime(__DIR__ . '/../../cache/random');
+    }
+    
+    
 
 }
