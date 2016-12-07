0afd72682578a1da4b6ca829faa711bf52c5946f
jeslopcru <jeslopcru@gmail.com>
2016-12-07 10:21:54 +0100

    03 ruta del directorio cache

--- src/CatApi/FileRandom.php
+++ src/CatApi/FileRandom.php
@@ -17,32 +17,34 @@ class FileRandom {
 
     const THREE_SECONDS_OF_LIFE = 3;
 
+    const CACHE_PATH = __DIR__ . '/../../cache/random';
+
     protected function fileHowOldIsIt() {
         echo "\n Tiempo del fichero ";
-        echo $time = time() - filemtime(__DIR__ . '/../../cache/random');
+        echo $time = time() - filemtime(self::CACHE_PATH);
         echo "\n";
         return $time;
     }
 
     protected function fileRandomExist() {
-        return file_exists(__DIR__ . '/../../cache/random');
+        return file_exists(self::CACHE_PATH);
     }
 
     public function conditionsToShowFileRandom() {
         $showPhoto = FALSE;
         if ($this->fileRandomExist() && $this->fileHowOldIsIt() <= self::THREE_SECONDS_OF_LIFE) {
-            $showPhoto = file_get_contents(__DIR__ . '/../../cache/random');
+            $showPhoto = file_get_contents(self::CACHE_PATH);
         }
         return $showPhoto;
     }
 
     public function showRandomFile() {
-        return file_get_contents(__DIR__ . '/../../cache/random');
+        return file_get_contents(self::CACHE_PATH);
     }
 
     public function insertIntoFileRandom(\SimpleXMLElement $responseElement) {
         file_put_contents(
-                __DIR__ . '/../../cache/random'
+                self::CACHE_PATH
                 , (string) $responseElement->data->images[0]->image->url
         );
     }
